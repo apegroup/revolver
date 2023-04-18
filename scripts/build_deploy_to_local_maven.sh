da71f652571fa505867c3d7dd46ee4daba22d16b
Cas van Luijtelaar <cas.luijtelaar@umain.com>
2023-04-18 12:38:57 +0200

    remove unused scripts

--- scripts/build_deploy_to_local_maven.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-set -e
-./gradlew clean publishAndroidReleasePublicationToMavenLocal
-
--- scripts/build_deploy_to_local_xcode.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-set -e
-./gradlew clean podspec podPublishReleaseXCFramework
-./gradlew kSwiftMultiplatformLibraryPodspec
-./gradlew copyFrameworksToXcodeProject
