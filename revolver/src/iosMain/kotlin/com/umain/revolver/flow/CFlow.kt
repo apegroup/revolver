2f68d5f9c20f020dc8dc0a7ff4071f8d05fe76cc
Alex Sergeev <Alex.sergeev@umain.com>
2024-10-08 00:47:25 +0200

    Upgrade to Kotlin 2.0

--- revolver/src/iosMain/kotlin/com/umain/revolver/flow/CFlow.kt
+++ revolver/src/iosMain/kotlin/com/umain/revolver/flow/CFlow.kt
@@ -5,6 +5,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.FlowCollector
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
@@ -13,6 +14,9 @@ actual open class CFlow<out T : Any> actual constructor(
     private val flow: Flow<T>,
     private val coroutineScope: CoroutineScope,
 ) : Flow<T> by flow {
+    actual override suspend fun collect(collector: FlowCollector<T>) {
+        flow.collect(collector)
+    }
 
     private fun watch(
         coroutineScope: CoroutineScope,
--- revolver/src/iosMain/kotlin/com/umain/revolver/flow/CStateFlow.kt
+++ revolver/src/iosMain/kotlin/com/umain/revolver/flow/CStateFlow.kt
@@ -1,11 +1,19 @@
 package com.umain.revolver.flow
 
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.FlowCollector
 import kotlinx.coroutines.flow.StateFlow
 
 actual open class CStateFlow<out T : Any> actual constructor(
     private val flow: StateFlow<T>,
     private val coroutineScope: CoroutineScope,
     ) : CSharedFlow<T>(flow,coroutineScope), StateFlow<T> {
-    override val value: T get() = flow.value
+    actual override val value: T get() = flow.value
+
+    actual override val replayCache: List<T>
+        get() = super.replayCache
+
+    actual override suspend fun collect(collector: FlowCollector<T>): Nothing {
+        super.collect(collector)
+    }
 }
